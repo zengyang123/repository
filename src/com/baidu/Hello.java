0c56dbad87c90897637cdbf5ff5afe488540e1a8
zengyang <zengyanglv@163.com>
2018-11-28 16:07:33 +0800

    init

---
+++ src/com/baidu/Hello.java
@@ -0,0 +1,27 @@
+package com.baidu;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        aaa();
+        System.out.println("HelloWorld");
+    }
+
+    public static void aaa() {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+    }
+}
